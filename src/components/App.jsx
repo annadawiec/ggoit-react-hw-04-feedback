1393514c28bbf4365c85b3a9ed5233027a869b11
annadawiec <annadawiec2022@gmail.com>
2023-07-21 10:55:29 +0200

    hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,82 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { Feedback } from './Feedback/Feedback';
 import Section from './Section/Section';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+//export class App extends Component {
+// state = {
+// good: 0,
+//neutral: 0,
+//bad: 0,
+//};
+
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const updateCountFeedback = event => {
+    //  this.setState(prevState => ({
+    // [value]: prevState[value] + 1,
+    // }));
+    const { name } = event.target;
+    switch (name) {
+      case 'good':
+        setGood(good + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+
+      case 'bad':
+        setBad(bad + 1);
+        break;
+
+      default:
+        break;
+    }
+  };
+  const countTotalFeedback = () => {
+    //Object.values(this.state).reduce((total, value) => total + value, 0);
+    return good + neutral + bad;
   };
 
-  updateCountFeedback = value => {
-    this.setState(prevState => ({
-      [value]: prevState[value] + 1,
-    }));
+  const countPositiveFeedbackPercentage = () => {
+    // return this.countTotalFeedback()
+    // ? ((this.state.good / this.countTotalFeedback()) * 100).toFixed(0)
+    //  : '0';
+    //};
+
+    const total = countTotalFeedback();
+
+    //render() {
+    //const { good, neutral, bad } = this.state;
+
+    return total ? ((good / total) * 100).toFixed(0) : '0';
   };
-  countTotalFeedback = () =>
-    Object.values(this.state).reduce((total, value) => total + value, 0);
+  const options = ['good', 'neutral', 'bad'];
 
-  countPositiveFeedbackPercentage = () => {
-    return this.countTotalFeedback()
-      ? ((this.state.good / this.countTotalFeedback()) * 100).toFixed(0)
-      : '0';
-  };
+  return (
+    <>
+      <Section title="Please, leave feedback">
+        <Feedback options={options} onFeedback={updateCountFeedback} />
+      </Section>
 
-  render() {
-    const { good, neutral, bad } = this.state;
-
-    return (
-      <>
-        <Section title="Please, leave feedback">
-          <Feedback
-            options={this.state}
-            onFeedback={this.updateCountFeedback}
+      {countTotalFeedback() ? (
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
         </Section>
-
-        {this.countTotalFeedback() ? (
-          <Section title="Statistics">
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          </Section>
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-      </>
-    );
-  }
-}
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+    </>
+  );
+};
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -2,17 +2,18 @@ import css from './Feedback.module.css';
 import PropTypes from 'prop-types';
 
 export const Feedback = ({ options, onFeedback }) => {
-  const stateData = Object.keys(options);
+  //const stateData = Object.keys(options);
 
   return (
     <div>
-      {stateData.map(option => (
+      {options.map(option => (
         <button
           className={css.button_feedback}
           key={option}
           type="button"
           name={option}
-          onClick={() => onFeedback(option)}
+          onClick={onFeedback}
+          //onFeedback(option)}
         >
           {option}
         </button>
